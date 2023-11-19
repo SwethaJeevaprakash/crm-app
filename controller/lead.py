2bf8900abcfef4f1dd2642ddc5d82cf5bf35a1c1
Swetha jagadeesh <swethajeeva95@gmail.com>
2023-11-19 05:22:32 +0000

    Implement lead update and delete functionality
    Written two functions, one for updating the lead resource and another
    for deleting the lead resource.

--- controller/lead.py
+++ controller/lead.py
@@ -3,3 +3,11 @@
 # Create lead.
 def create_lead():
   print 'Lead resource created.'
+
+# Update lead.
+def update_lead():
+  print 'Lead resource updated.'
+
+# Delete lead.
+def delete_lead():
+  print 'Lead resource deleted.'
