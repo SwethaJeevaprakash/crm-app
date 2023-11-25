2631a00b1985293d52c0d9b301363327a6a7391a
Swetha jagadeesh <swethajeeva95@gmail.com>
2023-11-25 05:15:28 +0000

    Make previous changes on dev-1 codebase: vim ~/normal-dev-1/crm-app/controller/lead.py

---
+++ controller/lead.py
@@ -0,0 +1,14 @@
+# Lead controller.
+
+# Create lead.
+def create_lead():
+  print 'Lead resource creation pushed to background worker.'
+  print 'Lead resource creation in progress.'
+
+# Update lead.
+def update_lead():
+  print 'Lead resource updated.'
+
+# Delete lead.
+def delete_lead():
+  print 'Lead resource deleted.'
